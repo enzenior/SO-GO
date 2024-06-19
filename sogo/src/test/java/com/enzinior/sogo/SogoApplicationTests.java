6ea8438d8cc286e012f2c7132b783ec93c38d366
hwang <hyb26@naver.com>
2024-06-19 13:32:39 +0900

    :tada: [Init] Create SOGO Project

---
+++ sogo/src/test/java/com/enzinior/sogo/SogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzinior.sogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
