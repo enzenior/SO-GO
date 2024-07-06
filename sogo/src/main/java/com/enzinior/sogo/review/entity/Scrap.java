054cdafa2dfed23fbbb615238b34e7e545a1e2b3
seungchul <seungchul0118@gmail.com>
2024-07-07 00:19:31 +0900

    :sparkles: [Feat] Scrap 기능 부분 추가(로직 추후 추가 예정)

---
+++ sogo/src/main/java/com/enzinior/sogo/review/entity/Scrap.java
@@ -0,0 +1,22 @@
+package com.enzinior.sogo.review.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Scrap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scrapId;
+
+//    @JoinColumn(name = "USER_ID")
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User user;
+
+    @JoinColumn(name = "REVIEW_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
+}
---
+++ sogo/src/main/java/com/enzinior/sogo/review/service/ScrapService.java
@@ -0,0 +1,5 @@
+package com.enzinior.sogo.review.service;
+
+public interface ScrapService {
+    void createScrap(String reviewUuid, String userUuid);
+}
