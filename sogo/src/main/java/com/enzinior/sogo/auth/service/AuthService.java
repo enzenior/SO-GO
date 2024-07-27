2969e0ee7a78264a696bbdf179dce3e9fb62c802
seungchul <seungchul0118@gmail.com>
2024-07-27 14:13:08 +0900

    Revert "⚡ [Chore] ServiceImpl 회원정보 삭제 수정, UserDto.Response 내 UserUuid 삭제"

--- sogo/src/main/java/com/enzinior/sogo/auth/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.enzinior.sogo.auth.service;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface AuthService {
-
-    void reissueToken(HttpServletRequest request, HttpServletResponse response);
-}
