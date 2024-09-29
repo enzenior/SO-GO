d5fb032da3f1293c50b08d541d55e7dc44982269
dahwan <sinphoto@naver.com>
2024-09-29 15:42:38 +0900

    :bug: [Fix] update SecurityConfig

--- sogo/src/main/java/com/enzinior/sogo/config/SecurityConfig.java
+++ sogo/src/main/java/com/enzinior/sogo/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/login", "/login", "/logout", "/api/auth/**", "/h2/**").permitAll()
+                        .requestMatchers("/api/login", "/login", "/logout", "/api/auth/**", "/h2/**", "/api/health/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                         .requestMatchers("/api/users/**").authenticated()
                         .requestMatchers("/api/reviews/my-reviews/*").authenticated()
